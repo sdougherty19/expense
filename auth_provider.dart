7030ef67c418ae37f71b143ca71ed0f3eebe62f8
sdougherty19 <sdougherty19@gmail.com>
2023-05-02 14:25:10 -0400

    Update auth_provider.dart

--- auth_provider.dart
+++ auth_provider.dart
@@ -18,10 +18,25 @@ class AuthProvider with ChangeNotifier {
       if (jsonString != null) {
         Map<String, dynamic> userData = json.decode(jsonString);
         _user = User.fromJson(userData);
+        await checkPassword(); // Add this line
         notifyListeners();
       }
     }
   }
+
+
+// --OLD Delete if code works
+  // Future<void> _loadUserData() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   if (prefs.containsKey('userData')) {
+  //     String? jsonString = prefs.getString('userData');
+  //     if (jsonString != null) {
+  //       Map<String, dynamic> userData = json.decode(jsonString);
+  //       _user = User.fromJson(userData);
+  //       notifyListeners();
+  //     }
+  //   }
+  // }
 
   Future<void> _saveUserData() async {
     if (_user != null) {
@@ -50,58 +65,21 @@ class AuthProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  User? get user => _user;
-}
+  Future<void> checkPassword() async {
+    if (_user == null) return;
+    final response = await http.post(
+      Uri.parse('https://appdata.netstoic.com/expense_rpt/check_password.php'), // replace with your API URL
+      body: {
+        'username': _user!.username,
+        'password': _user!.password,
+      },
+    );
 
+    final responseData = json.decode(response.body);
+    print('Server response: $responseData'); // Add this line
 
-// --Old Version Delete when final
-// class AuthProvider with ChangeNotifier {
-//   User? _user;
-//
-//   Future<void> login(String username, String password) async {
-//     final response = await http.post(
-//       Uri.parse('https://appdata.netstoic.com/expense_rpt/login.php'), // replace with your API URL
-//       body: {
-//         'username': username,
-//         'password': password,
-//       },
-//     );
-//
-//     final responseData = json.decode(response.body);
-//
-//     if (responseData['error'] != null) {
-//       throw Exception(responseData['error']);
-//     }
-//
-//     // Update this line to use User.fromJson factory method
-//     _user = User.fromJson(responseData);
-//     notifyListeners();
-//   }
-//
-//   User? get user => _user;
-// }
-// --Old Version Delete when final
-// class AuthProvider with ChangeNotifier {
-//   User? _user;
-//
-//   Future<void> login(String username, String password) async {
-//     final response = await http.post(
-//       Uri.parse('https://appdata.netstoic.com/expense_rpt/login.php'), // replace with your API URL
-//       body: {
-//         'username': username,
-//         'password': password,
-//       },
-//     );
-//
-//     final responseData = json.decode(response.body);
-//
-//     if (responseData['error'] != null) {
-//       throw Exception(responseData['error']);
-//     }
-//
-//     _user = User(username: username, name: responseData['name'], id: '');
-//     notifyListeners();
-//   }
-//
-//   User? get user => _user;
-// }
+    if (responseData['status'] == 'invalid') {
+      _user = null;
+      notifyListeners();
+    }
+  }
