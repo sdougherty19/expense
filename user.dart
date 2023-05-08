94cf01d3c161548dbf49304d1e895ae113f24319
sdougherty19 <sdougherty19@gmail.com>
2023-05-08 11:45:36 -0400

    Update user.dart

--- user.dart
+++ user.dart
@@ -27,28 +27,3 @@ class User {
     };
   }
 }
-
-// OLD Version Delete on completion
-// class User {
-//   final String id;
-//   final String name;
-//   final String username;
-//
-//   User({required this.id, required this.name, required this.username});
-//
-//   factory User.fromJson(Map<String, dynamic> json) {
-//     return User(
-//       id: json['id']?.toString() ?? '',
-//       name: json['name']?.toString() ?? '',
-//       username: json['username']?.toString() ?? '',
-//     );
-//   }
-//
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'id': id,
-//       'name': name,
-//       'username': username,
-//     };
-//   }
-// }
